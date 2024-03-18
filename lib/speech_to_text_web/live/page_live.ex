41d54187a23592b423229f7ceafb696454d038c0
nivetha11n <49003463+nivetha11n@users.noreply.github.com>
2024-03-18 14:39:28 +0100

    form creation

--- lib/speech_to_text_web/live/page_live.ex
+++ lib/speech_to_text_web/live/page_live.ex
@@ -1,13 +1,31 @@
 defmodule SpeechToTextWeb.PageLive do
   use SpeechToTextWeb, :live_view
+  import Phoenix.HTML
 
-  def mount(_param, _session, socket) do
-    {:ok, socket}
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, last_input: "")}
   end
+
 
   def render(assigns) do
     ~H"""
-    Hello world !!!
+    <h1>Input Form</h1>
+    <form phx-submit="handle_submit">
+      <input type="text" name="user_input" placeholder="Enter some text" />
+      <button type="submit">Submit</button>
+    </form>
+
+
+    <p>Last input: <%= @last_input %></p>
     """
-    end
+  end
+
+  def handle_event("handle_submit", %{"user_input" => user_input}, socket) do
+    # Handle the input value here. For now, let's just log it.
+    IO.inspect(user_input, label: "User Input")
+
+    # No changes to the socket, so we just return :noreply
+    {:noreply, assign(socket, last_input: user_input)}
+  end
+
 end
